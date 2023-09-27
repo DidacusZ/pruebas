1998a53d0b136863e8bf36f92354c3def2c85595
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-09-27 12:37:43 +0200

    ddd

--- pruebas/Program.cs
+++ pruebas/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("primera rama");
+            Console.WriteLine("primera rama");
+            Console.WriteLine("primera rama");
         }
     }
 }
